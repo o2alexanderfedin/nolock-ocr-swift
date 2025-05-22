7d665d5100e1aac04d62841339919f340d26b90b
o2alexanderfedin <github@o2.services>
2025-05-22 00:29:40 -0700

    chore: exclude MockingbirdExample.swift from test target to fix CI/CD build

--- Package.swift
+++ Package.swift
@@ -48,7 +48,7 @@ let package = Package(
                 .product(name: "Mockingbird", package: "mockingbird")
             ],
             path: "Tests",
-            exclude: ["README.md", "Mocks/README.md"],
+            exclude: ["README.md", "Mocks/README.md", "Mocks/MockingbirdExample.swift"],
             resources: [
                 .process("Resources")
             ]),
